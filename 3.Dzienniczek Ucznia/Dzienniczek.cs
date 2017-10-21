f2388c24dceafaafff3c36db5a2751b8720c44dc
Przemysław Szkaradek <przemeksacz@o2.pl>
2017-10-21 18:16:16 +0200

    3 Dzienniczek ucznia

---
+++ 3.Dzienniczek Ucznia/Dzienniczek.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Dzienniczek_Ucznia
+{
+    class Dzienniczek
+    {
+        // Stan (zmienne pola)
+        List<float> oceny;
+
+        // Zachowania - metody
+        public void DodajOcene(float ocena)
+        {
+            oceny.Add(ocena);
+        }
+
+        public float ObliczanieSredniej()
+        {
+            float suma = 0, srednia = 0;
+            foreach (var ocena in oceny)
+            {
+                suma = suma + ocena;
+            }
+
+            srednia = suma / oceny.Count();
+
+            return srednia;
+        }
+
+        public float PobierzMaxOcene()
+        {
+            return oceny.Max();
+        }
+
+        public float PobierzMinimalnaOcene()
+        {
+            return oceny.Min();
+        }
+    }
+}
---
+++ DzienniczekUcznia/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DzienniczekUcznia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
