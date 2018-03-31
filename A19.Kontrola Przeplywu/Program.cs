7487feafe5bf771f965c116a5cf71a0bb646f31a
Przemysław Szkaradek <przemeksacz@o2.pl>
2018-03-31 17:21:50 +0200

    A19.Kontrola Przeplywu Warunek

---
+++ A19.Kontrola Przeplywu/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A19.Kontrola_Przeplywu
+{
+    class Program
+    {
+        private static int wiek;
+        private static string imie;
+
+
+        //Kontrola przepływu - Rozgałęzienia - If (Warunek).
+
+        // if (age <= 10)
+        //    Method1();
+        //else if (age< 20)
+        //    Method2();
+        //Jeśli żaden wcześniejszy warunek nie został spełniony
+        //else
+        //Nawiasy klamrowe są wymagane gdy mamy wiecej metod - tutaj akurat zastosowaliśmy nawiasy dla czytelności kodu.
+        //{
+        //    Method3();
+        //}
+        // 
+        //Operator 3 argumentowy. Jezeli wiek jest wiekszy od 18 Pełnoletni w przeciwnym wypadku Dziecko. - Szybkie Sprawdzenie.
+        //string test = age > 18 ? "Pełnoletni" : "Dziecko";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swój wiek: ");
+            wiek = int.Parse(Console.ReadLine());
+            //Operator 3 argumentowy
+            string test = wiek > 18 ? "Pełnoletni" : "Dziecko";
+            Console.WriteLine(test);
+            Console.ReadKey();
+
+            Console.WriteLine("Podaj swój wiek: ");
+            wiek = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj swoje imię: ");
+            imie = Console.ReadLine();
+            if (wiek <= 10)
+            {
+                if (imie == "Marcin")
+                {
+                    Metoda1();
+                }
+
+            }
+            else if (wiek < 20)
+            {
+                Metoda2();
+            }
+            else if (wiek < 30)
+            {
+                Metoda3();
+            }
+            //Jeśli żaden wcześniejszy warunek nie został spełniony - else      
+            else
+            {
+                Metoda4();
+            }
+            Console.ReadKey();
+        }
+
+        private static void Metoda4()
+        {
+            Console.WriteLine("Masz 30 i więcej lat");
+        }
+
+        private static void Metoda3()
+        {
+            Console.WriteLine("Twój wiek większy lub równy 20 a mniejszy od 30 i wynosi " + wiek);
+        }
+
+        private static void Metoda2()
+        {
+            Console.WriteLine("Twój wiek większy od 10 a mniejszy od 20 i wynosi " + wiek);
+        }
+
+        private static void Metoda1()
+        {
+            Console.WriteLine("Masz 10 lub mniej lat a dokładnie masz " + wiek);
+        }
+    }
+}
---
+++ A19.Kontrola Przeplywu/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("A19.Kontrola Przeplywu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("A19.Kontrola Przeplywu")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eabc55f5-b984-4808-84c5-769e16559a10")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
